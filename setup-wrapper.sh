f10d073a65d8d96b0cde59f864ea3a2976df66a8
LarsPenguin <grizzlybln123@gmail.com>
2026-06-07 16:09:51 +0200

    Delete setup-wrapper.sh

--- setup-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p gradle/wrapper
-curl -L -o gradle/wrapper/gradle-wrapper.jar \
-  https://repo.gradle.org/gradle/libs-releases-local/org/gradle/gradle-wrapper/8.6/gradle-wrapper-8.6.jar
-git add gradle/wrapper/gradle-wrapper.jar
-git commit -m "Add gradle-wrapper.jar"
-git push
